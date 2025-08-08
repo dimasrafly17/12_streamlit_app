a949728e31c2601ffdbc64bd602056bb520d2f5a
dimasrafly17 <dimas.abimanyu@banksampoerna.com>
2025-08-09 01:41:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,19 @@ import pandas as pd
 # Load data
 data = pd.read_csv('customer_profile.csv')  # Ganti dengan path/URL data kamu
 
+# Data Cleaning
+# Bersihkan simbol mata uang, titik pemisah ribuan, dan ganti koma jadi titik (kalau perlu)
+data['FUM'] = (
+    data['FUM']
+    .astype(str)
+    .str.replace(r'[^\d,.-]', '', regex=True)
+    .str.replace(',', '.')
+)
+
+# Lalu ubah ke numerik
+data['FUM'] = pd.to_numeric(data['FUM'], errors='coerce')
+
+
 st.title("Branch Dashboard")
 
 # Dropdown untuk CABANG
@@ -34,4 +47,4 @@ st.metric("Total CIF", nunique_customer)
 st.metric("Total Account", nunique_account)
 st.metric("New CIF", new_cif_count)
 st.metric("New REK", new_acct_count)
-st.metric("Total FUM", fum)
+st.metric("Total FUM", f"Rp {fum:,.2f}")
